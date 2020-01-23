0a27b0d578d5d4fa8c37834cf9188b2e0dec811b
jpcarroll <jpcarrrolll@gmail.com>
2020-01-23 17:40:26 -0500

    updated schedule and prizes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import moment from 'moment';
+import * as saturday from './Saturday.json';
+import * as sunday from './Sunday.json';
+import * as prizes from './Prizes.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit {
   timetohackathon = moment.duration(moment().diff(moment("January 22, 2020"))).humanize()
+  saturday = (saturday as any).default;
+  sunday = (sunday as any).default;
+  prizes = (prizes as any).default;
+
+  ngOnInit() {
+    for (let i = 9; i <= 21; i+=2) {
+      let t:string;
+      if ( i > 11)
+        t = (i - 12) + ":00 PM"
+      else
+        t = i + ":00 AM"
+      this.clock.push(t);
+    }
+  }
+
+  public greaterThan(i: number, num: number) {
+    return i < num;
+  }
+
+  clock = []
 }
