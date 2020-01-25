43384c00bdb381d80d3651db2046b4b5e4ecc5ff
Ayush Petigara <aarvyushpetigara3@gmail.com>
2020-01-25 10:43:27 -0500

    auto refresh the clock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,6 @@ export class AppComponent implements OnInit {
   timetohackathon = moment() < moment("2020-01-25 10:00:00") ?
     moment.duration(moment("2020-01-25 10:00:00").diff(moment())).format("h [Hours], m [Minutes]") : 
     moment.duration(moment("2020-01-26 10:00:00").diff(moment())).format("h [Hours], m [Minutes]");
-
   saturday = (saturday as any).default;
   sunday = (sunday as any).default;
   prizes = (prizes as any).default;
@@ -31,6 +30,7 @@ export class AppComponent implements OnInit {
         t = i + ":00 AM"
       this.clock.push(t);
     }
+    
   }
 
   public greaterThan(i: number, num: number) {
